553bf03a0be64bfe2fef46b781e50ddbb78174b1
Irfan <irfanhayat55@gmail.com>
2021-01-26 17:58:50 +0500

    post and get using mongodb connection

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,28 +1,19 @@
-import { Controller,Get,Post, Put,Delete } from '@nestjs/common';
+import { Controller,Get,Post, Put,Delete, Param,Body } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
 import {UserService} from './user.service'
 
 @Controller('user')//A controller's purpose is to receive specific requests for the application. 
 export class UserController {
 constructor(private readonly userService:UserService){} //Dependencies are services or objects that a class needs to perform its function. Dependency injection, or DI, is a design pattern in which a class requests dependencies from external sources rather than creating them
 
-@Get('getAllUser')
-getAllUser(id:string){
+@Get('getAllUser/:id')
+getAllUser(@Param('id') id:Promise<User>){
     return this.userService.getSingleUser(id);
 }
 
 @Post('createSingleUser')
-createSingleUser():string{
-    return this.userService.createSingleUser();
-}
-
-@Put('updateSingleUser')
-updateSingleUser():string{
-    return this.userService.updateSingleUser();
-}
-
-@Delete('updateSingleUser')
-deleteSingleUser():string{
-    return this.userService.deleteSingleUser();
+createSingleUser(@Body() user:User):Promise<User>{
+    return this.userService.createSingleUser(user);
 }
 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,27 +2,22 @@ import { Injectable } from '@nestjs/common';
 import  {User} from './interfaces/user.interface';
 import {Model} from 'mongoose';
 import {InjectModel} from '@nestjs/mongoose';
+import { UserSchema } from './schemas/user.schema';
 
 @Injectable()  
 export class UserService  {
  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
 
- async getSingleUser(id:string):Promise<User> {
+ async getSingleUser(id):Promise<User> {
     return await this.userModel.findOne({_id:id}) 
 }
 
-createSingleUser():string{
-    return 'Create Single User'
+async createSingleUser(user):Promise<User>{
+     let new_user=new this.userModel(user)
+     return new_user.save();
 }
 
-updateSingleUser():string{
-    return 'Update Single User'
-}
-
-deleteSingleUser():string{
-    return 'Delete Single User'
-}
 
 
 
